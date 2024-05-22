335d134bb42a9f7cf52c0f4af2ea5a8c9e8878db
mustafauzum <mustafauzum05@gmail.com>
2024-05-22 03:00:57 +0300

    contectcommit

---
+++ src/components/Profile.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+import {useUser} from "../context/UserContext"
+
+function Profile() {
+    const {user, setUser} = useUser()
+
+    const [loading, setLoading] = useState(false)
+
+    const handleLogin = () => {
+        setLoading(true)
+        setTimeout(() => {
+            setUser({id: 37, username:"mustafazm", bio: "I like Latitude"})
+            setLoading(false)
+        }, 1500);
+    }
+
+    const handleLogout = () => {
+        setUser(null)
+    }
+
+    return <div>
+        {
+            !user && (<button onClick={handleLogin}>{loading ? "loading...": "Login"}</button>
+        )}
+        
+        <br />
+        <code>{JSON.stringify(user)}</code>
+        <br />
+        {
+            user && <button onClick={handleLogout}>Logout</button>
+        }
+    </div>
+}
+
+export default Profile
